96f3c3d1d3f2368b5710aaddb1c7f9c01a040175
Spencer Moody <spencer.moody@outlook.com>
2020-08-17 11:18:05 -0700

    Create Initial Migrations
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

---
+++ MessageBoard/Migrations/20200817181618_Initial.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    BoardId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.BoardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Heading = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    DatePosted = table.Column<DateTime>(nullable: false),
+                    BoardId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_Messages_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "BoardId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_BoardId",
+                table: "Messages",
+                column: "BoardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+        }
+    }
+}
--- MessageBoard/Models/Message.cs
+++ MessageBoard/Models/Message.cs
@@ -1,1 +1,14 @@
-namespace MeesageBoard.Models
+using System;
+
+namespace MessageBoard.Models
+{
+  public class Message
+  {
+    public int MessageId { get; set; }
+    public string Heading { get; set; }
+    public string Body { get; set;}
+    public DateTime DatePosted { get; set; }
+    public int BoardId { get; set; }
+    public virtual Board Board { get; set; }
+  }
+}
