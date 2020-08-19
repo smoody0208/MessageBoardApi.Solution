cf4b322f4a2eb29853aacce0fdf91ca002560522
Spencer Moody <spencer.moody@outlook.com>
2020-08-19 13:36:56 -0700

    add data seeding for new Messages and Posts
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

---
+++ MessageBoard/Migrations/20200819201616_PostClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class PostClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Boards_BoardId",
+                table: "Messages");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.RenameColumn(
+                name: "BoardId",
+                table: "Messages",
+                newName: "PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_BoardId",
+                table: "Messages",
+                newName: "IX_Messages_PostId");
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    BoardId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    DatePosted = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "BoardId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BoardId",
+                table: "Posts",
+                column: "BoardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Posts_PostId",
+                table: "Messages",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "PostId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Posts_PostId",
+                table: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.RenameColumn(
+                name: "PostId",
+                table: "Messages",
+                newName: "BoardId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_PostId",
+                table: "Messages",
+                newName: "IX_Messages_BoardId");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "Password", "Role", "Token", "Username" },
+                values: new object[] { 4, "Normal", "User", "user", "User", null, "user" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Boards_BoardId",
+                table: "Messages",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "BoardId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
